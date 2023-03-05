c8fd5b17a0f4edfaf3d949265dc7eaccd35fbee1
othAs-dev <othmane.developer@gmail.com>
2023-03-05 02:26:15 +0100

    fix email

--- src/pages/api/sendContact.js
+++ src/pages/api/sendContact.js
@@ -97,4 +97,5 @@ export default async function handler(req, res) {
 
     res.status(500).json({ error: "Failed to submit form. Please try again." });
   }
+  return transporter.sendMail(mailData);
 }
