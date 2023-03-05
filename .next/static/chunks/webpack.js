a28a4123abc29b7cd3d57b3e92ee3dd58805b716
othAs-dev <othmane.developer@gmail.com>
2023-03-05 17:20:41 +0100

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec737ca273f8677f"; }
+/******/ 		__webpack_require__.h = function() { return "525fdbcd73e800ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
