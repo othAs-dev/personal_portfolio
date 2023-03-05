bc3fc3620c856b559a3e7bad66d1521c1097682e
othAs-dev <othmane.developer@gmail.com>
2023-03-05 02:16:54 +0100

    fix email

--- src/pages/api/sendContact.js
+++ src/pages/api/sendContact.js
@@ -27,6 +27,9 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
+      tls: {
+        ciphers: "SSLv3",
+      },
       secure: true,
       auth: {
         user: process.env.MAIL_USER,
